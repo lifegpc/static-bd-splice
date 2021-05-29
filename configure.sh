326dc33b340b325e68a6fee0c16806d89ace9c89
lifegpc <g1710431395@gmail.com>
2021-05-29 22:58:44 +0800

    also add for x64

--- configure.sh
+++ configure.sh
@@ -3,7 +3,8 @@ loc=`pwd`/bluray
 mkdir -p bluray
 cd libbluray
 autoreconf -vi
-FLAGS=--enable-shared=no
+FLAGS=--disable-bdjava-jar
+FLAGS+=' --enable-shared=no'
 FLAGS+=' --enable-static=yes'
 FLAGS+=" --prefix=$loc"
 FLAGS+=' LDFLAGS=-static'
