3ba07b3139b94ad5588c1ea890623325ceddbce5
lifegpc <g1710431395@gmail.com>
2021-05-29 22:58:10 +0800

    disable bdjava jar

--- configure_x86.sh
+++ configure_x86.sh
@@ -3,7 +3,8 @@ loc=`pwd`/bluray-x86
 mkdir -p bluray-x86
 cd libbluray
 autoreconf -vi
-FLAGS=--enable-shared=no
+FLAGS=--disable-bdjava-jar
+FLAGS+=' --enable-shared=no'
 FLAGS+=' --enable-static=yes'
 FLAGS+=" --prefix=$loc"
 FLAGS+=' LDFLAGS=-static'
